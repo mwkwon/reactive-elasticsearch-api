bb5a104774dbc93ecd155545096d6637b6daf556
Min Wook Kwon <mwkwon@joonggonara.co.kr>
2022-06-26 21:01:29 +0900

    init project

---
+++ src/main/java/io/mwkwon/reactive/elasticsearch/api/ReactiveElasticsearchApiApplication.java
@@ -0,0 +1,13 @@
+package io.mwkwon.reactive.elasticsearch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveElasticsearchApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveElasticsearchApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/mwkwon/reactive/elasticsearch/api/ReactiveElasticsearchApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mwkwon.reactive.elasticsearch.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveElasticsearchApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
